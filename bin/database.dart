17573c01228b57e130367bb91bf93e599aa3f138
estebanCaballero70200 <e.caballero.devolop@gmai.com>
2021-02-05 16:10:08 -0300

    crud ususario terminado

--- bin/database.dart
+++ bin/database.dart
@@ -1,13 +1,71 @@
 import 'package:mongo_dart/mongo_dart.dart';
-import 'dart:io' show Platform;
+import 'package:min_id/min_id.dart';
+import 'package:intl/intl.dart';
 
 class Database {
-  String host = Platform.environment['MONGO_DART_DRIVER_HOST'] ?? '127.0.0.1';
-  String port = Platform.environment['MONGO_DART_DRIVER_PORT'] ?? '27017';
+  final user = 'dev70200';
+  final pass = 'dev70200';
 
   void dbconection() async {
     var db = await Db.create(
-        "mongodb+srv://<user>:<password>@<host>:<port>/<database-name>?<parameters>");
+        'mongodb+srv://$user:$pass@cluster0.dmffq.mongodb.net/morfiDB?retryWrites=true&w=majority');
     await db.open();
+
+    db.isConnected
+        ? print('base de datos conectada')
+        : print('no hay coneccion con base de datos');
+
+    DbCollection coll = db.collection('usuario');
+
+    //************LISTAR USUARIOS */
+
+    /* var usuario =
+        await coll.find().toList(); */
+    //print(usuarios);
+
+    //await db.close();
+
+    //print(usuarios);
+
+    //************CREAR USUARIOS */
+
+    /* final date = DateTime.now();
+    final dateformated =
+        DateFormat('dd.MM.yyyy HH:mm').format(DateTime.parse(date.toString()));
+
+    print(dateformated);
+
+    final id = MinId.getId('{{3{d}}{3{w}}{3{d}}{3{w}}{3{d}}{3{w}}{3{d}}{3{w}}}')
+        .toString();
+    await coll.save({
+      '_id': id,
+      'name': 'Esteban Caballero',
+      'email': 'esteban.caballero@70200.uy',
+      'phone': '+598 94387200',
+      'address': 'continucion 18 de julio 521',
+      'registered': '2019-05-18T08:38:00 +03:00',
+      'latitude': -41.034292,
+      'longitude': 123.818295,
+      'isClient': true,
+      'isDelivery': false
+    });
+
+    print('usuario registrado con el id $id'); */
+
+    //************ACTUALIZAR USUARIOS */
+
+    /* await coll.update(
+        await coll.findOne(where.eq('_id', '703oOW177rzN523vvn823qxv')), {
+      r'$set': {'registered': dateformated},
+    });
+
+    print('usuario modificado');
+ */
+    //************BORRAR USUARIOS */
+
+    /*  await coll.remove(where.eq('_id', '703oOW177rzN523vvn823qxv'));
+    print('usuario eliminado');
+  */
+    //await db.close();
   }
 }
--- bin/server.dart
+++ bin/server.dart
@@ -1,27 +1,83 @@
-import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+import 'package:mongo_dart/mongo_dart.dart';
+import 'package:min_id/min_id.dart';
+import 'package:intl/intl.dart';
 
-import 'package:socket_io/socket_io.dart';
+void main() async {
+  final user = 'dev70200';
+  final pass = 'dev70200';
+  var port = 8085;
 
-import 'database.dart';
+  var db = await Db.create(
+      'mongodb+srv://$user:$pass@cluster0.dmffq.mongodb.net/morfiDB?retryWrites=true&w=majority');
+  await db.open();
 
-void main() {
-  final server = Server();
-  final database = Database();
+  db.isConnected
+      ? print('base de datos conectada')
+      : print('no hay coneccion con base de datos');
 
-  // el servidor escucha si hay clientes conectados
-  server.on('connection', (client) {
-    print('Connect to $client ');
+  DbCollection coll = db.collection('usuario');
 
-    // el servidor escuhando al cliente.
-    client.on('stream', (data) {
-      print('Data from client: $data');
+  var server = await HttpServer.bind('localhost', port);
 
-      // el servidor enviando un mensaje de vuelta una vez recibe un mensaje del cliente
-      client.emit('msg', 'me enviaste este mensaje: $data');
-    });
+  server.listen((HttpRequest request) async {
+    print('escuchando el servidor');
+    ContentType contentType = request.headers.contentType;
+    switch (request.uri.path) {
+      case '/':
+        request.response.write('estoy en la raiz');
+        break;
+      case '/users':
+
+        //TODO: handle GET request
+        if (request.method == 'GET') {
+          request.response.write(await coll.find().toList());
+        }
+        //TODO: handle POST request
+        else if (request.method == 'POST') {
+          final content =
+              await request.cast<List<int>>().transform(Utf8Decoder()).join();
+          final data = json.decode(content);
+          await coll.save(data);
+        }
+
+        //TODO: handle PUT request
+        else if (request.method == 'PUT') {
+          var id = await request.uri.queryParameters['id'];
+          final content =
+              await request.cast<List<int>>().transform(Utf8Decoder()).join();
+          final data = json.decode(content);
+          var itemToReplace = await coll.findOne(where.eq('_id', id));
+
+          if (itemToReplace == null) {
+            await coll.save(data);
+          } else {
+            await coll.update(itemToReplace, data);
+          }
+        }
+        //TODO: handle DELETE request
+        else if (request.method == 'DELETE') {
+          var id = await request.uri.queryParameters['id'];
+          await coll.remove(where.eq('_id', id));
+        }
+        //TODO: handle PATCH request
+        else if (request.method == 'PATCH') {
+          var id = await request.uri.queryParameters['id'];
+          final content =
+              await request.cast<List<int>>().transform(Utf8Decoder()).join();
+          final data = json.decode(content);
+          var itemToPatch = await coll.findOne(where.eq('_id', id));
+          await coll.update(itemToPatch, {r'$set': data});
+        }
+        ;
+        break;
+      default:
+        request.response
+          ..statusCode = HttpStatus.notFound
+          ..write('Not Found');
+    }
+
+    await request.response.close();
   });
-
-  database.dbconection;
-
-  server.listen(3005);
 }
